d97c88309b572614bddb79b9431a86414981d3da
lyskovrocks <lyskov.alex@gmail.com>
2023-05-15 20:25:57 +0300

    Доделана последняя домашка

--- todoapp/views.py
+++ todoapp/views.py
@@ -6,27 +6,66 @@ from todoapp.models import TodoStatus, TodoTask, TodoList
 
 # Create your views here.
 
+
+
 def todo_main(request):
     if request.method == 'POST':
         action_type = request.POST.get('type')
         if action_type == 'create':
-            todo = TodoTask()
-            todo.title = request.POST.get('title')
-            todo.status = TodoStatus.objects.get(pk=TodoStatus.C_NOT_COMPLETED)
-            todo.create_at = datetime.now()
-            todo.text = ''
-            todo.todo_list = TodoList.objects.get(pk=1)
-            todo.save()
+            __create_task(request)
+
+        if action_type == 'clear':
+            __clear_completed(request)
+
         if action_type == 'destroy':
-            task_id = request.POST.get('task_id')
-            TodoTask.objects.get(pk=task_id).delete()
+            TodoTask.objects.get(pk=request.POST.get('task_id')).delete()
+    else:
+        action_type = request.GET.get('type')
+        if action_type == 'change':
+            __chang_todo_status(request)
+            
+    tasks = TodoTask.objects.filter(todo_list__user=request.user)
+    if request.GET.get('status') == 'yes':
+        tasks = tasks.filter(status_id=TodoStatus.C_COMPLETED)
+    elif request.GET.get('status') == 'no':
+        tasks = tasks.filter(status_id=TodoStatus.C_NOT_COMPLETED)
 
-
-
-    todo_list = TodoTask.objects.all().order_by('-id')
     return render(request, 'todo_main.html', {
-        'tasks_array': todo_list,
-        'todo_count': todo_list.count()
+        'tasks_array': tasks.order_by('-id'),
+        'todo_count': tasks.count()
     })
+
+def __chang_todo_status(request):
+    task = TodoTask.objects.get(pk=request.GET.get('task_id'))
+    if task.status == TodoStatus.objects.get(pk=TodoStatus.C_NOT_COMPLETED):
+        task.status = TodoStatus.objects.get(pk=TodoStatus.C_COMPLETED)
+    else:
+        task.status = TodoStatus.objects.get(pk=TodoStatus.C_NOT_COMPLETED)
+    task.save()
+
+def __create_task(request):
+    print('*********************Start creating task**********************')
+    todo_list = TodoList.objects.filter(user=request.user).first()
+    if todo_list is None:
+        print('*********************todo_list is None**********************')
+        todo_list = TodoList(user=request.user, date=datetime.now())
+        todo_list.save()
+        print('New todolist was created')
+
+    todo = TodoTask()
+    todo.title = request.POST.get('title')
+    todo.status = TodoStatus.objects.get(pk=TodoStatus.C_NOT_COMPLETED)
+    # 1todo.create_at = datetime.now()
+    todo.text = ''
+    todo.todo_list = todo_list
+    todo.save()
+
+def __clear_completed(request):
+    tasks = TodoTask.objects\
+        .filter(todo_list__user=request.user)\
+        .filter(status_id=TodoStatus.C_COMPLETED)\
+        .delete()
+    pass
+
 
 
